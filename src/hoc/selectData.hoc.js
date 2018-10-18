4c678131815c97ddc7af6542c4576169b5a91ceb
Sammy el dueno <sammyeldueno@imac-de-sammy.home>
2018-10-18 12:42:23 +0200

    fix(all app) => erreur hoc suite variable placée après condition if du componentdidupdate description : variable -RecipesList- remontée au dessus de la condition

--- src/hoc/selectData.hoc.js
+++ src/hoc/selectData.hoc.js
@@ -1,40 +1,32 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 const selectData = (WrappedComponent, action) => {
+  class findRecipe extends Component {
+    state = {
+      data: []
+    };
 
-    class findRecipe extends Component{
-
-        state= {
-            data: [],
-        }
-
-    async componentDidMount(){
-        
-        let RecipesList = await action();
-		this.fillData(RecipesList);
+    async componentDidMount() {
+      let RecipesList = await action();
+      this.fillData(RecipesList);
     }
 
-    async componentDidUpdate(prevState){
-       if(prevState.data !== this.state.data)
-       let RecipesList = await action();
-		this.fillData(RecipesList);
+    async componentDidUpdate(prevState) {
+      let RecipesList = await action();
+      if (prevState.data !== this.state.data) return this.fillData(RecipesList);
     }
 
-    fillData(result){
-
-        this.setState({
-            data: result
-        })
+    fillData(result) {
+      this.setState({
+        data: result
+      });
     }
 
-    render(){
-
-        return <WrappedComponent  content={this.state.data}/>
+    render() {
+      return <WrappedComponent content={this.state.data} />;
     }
-}
-    return findRecipe;
-
+  }
+  return findRecipe;
 };
 
 export default selectData;
