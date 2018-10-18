7322f3a12d94a8884934f322601496097e510329
Sammy el dueno <sammyeldueno@imac-de-sammy.home>
2018-10-18 13:45:03 +0200

    refactor(homepage) => deplacement de la background video vers styles/

--- src/component/homepage/Homepage.js
+++ src/component/homepage/Homepage.js
@@ -1,41 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 // import video
-import styles from './scss/homepage.all.scss'
+import styles from "./scss/homepage.all.scss";
 
-import food from '../../cooking.mp4';
 // Import HOC
-import selectData from '../../hoc/selectData.hoc';
+import selectData from "../../hoc/selectData.hoc";
 
 // Import API
-import { getRecipes } from '../../utils/api';
+import { getRecipes } from "../../utils/api";
 
-const HomePage = ({ content }) =>{
-   console.log(content)
-    return(
+const HomePage = ({ content }) => {
+  console.log(content);
+  return (
+    <section>
+      {content.map(({ recipe }) => {
+        return <p>{recipe.label}</p>;
+      })}
+    </section>
+  );
+};
 
-        <section>
-            {content.map( ({recipe}) => {
-                return(
-                <p>{recipe.label}</p>
-                )
-            })}
-
-        <div className="shadow">
-        <button>ENTER</button>
-        </div>
-
-        <video autoPlay id="video-background" muted>
-        <source src={food} type="video/mp4"/>
-        <source src={ food } type="video/ogg"/>
-        </video>
-
-        </section>
-    )
-
-}
-
-const Home = selectData(HomePage, () => getRecipes('chicken','potatoes') );
+const Home = selectData(HomePage, () => getRecipes("chicken", "potatoes"));
 
 export default Home;
