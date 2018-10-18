0201abefe5a98960cb60962e91d2c4764b748f1b
Sammy el dueno <sammyeldueno@imac-de-sammy.home>
2018-10-18 12:39:25 +0200

    refactor(header)=> ajouter background video via un component description: creation d'un component fournissant une video en fullscreen pour la page d'accueil de l'app

--- src/component/layout/Header.js
+++ src/component/layout/Header.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
+// import background video
+
+import VideoBackground from "../../styles/Background.video";
 
 const Header = () => (
-
-    <header>
-        <h1><span>FOOD</span>2TOI</h1>
-    </header>
+  <header>
+    <VideoBackground />
+    <h1>
+      <span>FOOD</span>
+      2TOI
+    </h1>
+  </header>
 );
 
 export default Header;
