8e543f7352c1a0f968aafd46ecdbe64a7d4e45d8
WebWalterWrite <webwalterwhrite@gmail.com>
2019-04-23 14:41:45 +0200

    -- nc --

---
+++ src/styles/responsive.styled.js
@@ -0,0 +1,23 @@
+import {css} from 'styled-components';
+
+
+const sizes = {
+    desktopL : 2500,
+    desktopM: 1440,
+    tablet: 768,
+    phone: 576,
+}
+
+export const media = Object.keys(sizes).reduce((acc, label) => {
+    acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)}
+    }
+  `
+    return acc
+}, 
+{})
+
+
+
+
