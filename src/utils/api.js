8a81b962b07217a9967d43f15dadc673200c753f
WebWalterWrite <webwalterwhrite@gmail.com>
2019-04-23 14:47:21 +0200

    type: api desc: --

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API_ID = process.env.REACT_APP_FOOD_API_ID;
 const API_KEY = process.env.REACT_APP_FOOD_API_KEY;
 const URL = process.env.REACT_APP_FOOD_API_URL;
-
 
 /* Pour rechercher avec un ou plusieurs ingrédients.
     parametre de fonction => "q" prend 1 ou plusieurs paramètres ( a ou a+b ou a+b+c ...): 
@@ -14,13 +13,12 @@ const URL = process.env.REACT_APP_FOOD_API_URL;
 */
 
 export const getRecipes = async (...data) => {
- 
-    // concatener les ingrédients si plusieurs choix.
-    let choice = data.join('+');
+  // concatener les ingrédients si plusieurs choix.
+  let choice = data.join("+");
 
-
-    const result = await axios.get(`${URL}?q=${choice}&app_id=${API_ID}&app_key=${API_KEY}&from=0&to=20`)
-    console.log(result)
-    return result.data.hits;
-
+  const result = await axios.get(
+    `${URL}?q=${choice}&app_id=${API_ID}&app_key=${API_KEY}&from=0&to=20`
+  );
+  console.log(result);
+  return result.data.hits;
 };
