9806c2e6d5fcdf7d4b12ad8c33c9ec75a8927c2f
WebWalterWrite <webwalterwhrite@gmail.com>
2019-04-23 14:32:00 +0200

    type: routing desc: add routes file

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+// import component
+
+import Homepage from './component/homepage/Homepage';
+
+const AllRoutes = [
+    {
+        path:'/',
+        component: Homepage
+    }
+];
+
+export default AllRoutes;
