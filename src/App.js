43417dfe0d39aeec2ac969e0d645671bf58629fb
WebWalterWrite <webwalterwhrite@gmail.com>
2019-04-23 14:36:51 +0200

    ajout routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Fragment }  from 'react';
-// Import components
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { createGlobalStyle } from "styled-components";
 
-import Header from './component/layout/Header';
-import Home from './component/homepage/Homepage';
+
+// Import components
+import Header from './component/layout/header/Header';
+import AllRoutes from './routes';
+
+// import mixins for sass
  
 const Layout = ({children}) => {
   return(
   <Fragment>
     <Header/> 
-      {children}
-      <footer></footer>
+      <main>
+        {children}
+      </main>
+  <GlobalStyle/>
   </Fragment>
 
   )
@@ -17,19 +24,34 @@ const Layout = ({children}) => {
 };
 
 
-const App = () => (
+const Routes = () => (
 
-  <Layout>p
-    
-    <main>
-      <Home />
-    </main>
+  <Layout>
+      {AllRoutes.map( ({path, component}, k)=> <Route key={k} path={path} component={component} />)}
   </Layout>
 )
 
+const App = () => <Router><Switch><Route  exact path='/' component={Routes} /></Switch></Router>
 
+const GlobalStyle = createGlobalStyle`
+*{
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+    };
+body {
+  height: 100%;
+  width:100%;
+};
+body{
+   background-color: black;
+   font-size :1.4rem;
+};
+p{
+  letter-spacing: 3px;
+}
 
-
+`;
 
 
 
