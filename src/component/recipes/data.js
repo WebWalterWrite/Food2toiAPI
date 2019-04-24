aeaeadef9bcb341aaaced2bd2c2a88bd4e702261
WebWalterWrite <webwalterwhrite@gmail.com>
2019-04-24 16:33:49 +0200

    type: data desc: ajout id product

--- src/component/recipes/data.js
+++ src/component/recipes/data.js
@@ -18,8 +18,9 @@ const vegetable = [
  * @return {Array} - Tableau d'objects
  */
 const vegetables = vegetable.map( v => ({
+		id: `v_${v.substr(0,3)}_${Math.floor(Math.random() * (30 - 10 + 1)) + 10}`,
         name: v,
-        image: images(`./${v.split(" ").join("")}.jpg`)
+        image: images(`./${v.split(" ").join("")}.jpg`),
     })
  )
 
