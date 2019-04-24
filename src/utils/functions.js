e7019f78a1eb924b1afcb22892ef3f89a4cfe4a3
WebWalterWrite <webwalterwhrite@gmail.com>
2019-04-24 16:20:10 +0200

    type: utils desc: add functions localstorage

---
+++ src/utils/functions.js
@@ -0,0 +1,31 @@
+
+// Récupérer le contenu du localstorage
+const getLocalStorage = item => JSON.parse(localStorage.getItem(item));
+
+// persister localstorage
+const setLocalStorage = (item, array) => localStorage.setItem(item, JSON.stringify(array));
+
+/**
+ * 
+ * @param {string} item - La clé localStorage
+ * @param {object} p - Contient les données de l'ingrédient séléctionné.
+ */
+export const storeData = (item, p) => {
+
+    // Vérifier si des données existent si non créer un nouveau array.
+    let arrayOfProducts = getLocalStorage(item) ? getLocalStorage(item) : [];
+
+    // Récupérer le array
+    let getStorage = getLocalStorage(item);
+
+
+    // Si getStorage true, Vérifier si le produit est déjà présent
+    const isProduct = getStorage && getStorage.find(({ id }) => id === p.id);
+
+    // Si non présent l'insérer dans le tableau
+    !isProduct && arrayOfProducts.push(p);
+
+    // Persister les données 
+    setLocalStorage(item,arrayOfProducts);
+
+};
