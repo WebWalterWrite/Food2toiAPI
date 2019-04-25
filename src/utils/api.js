56b8983c9f30954b05f0ce723ea93b5e008f82d8
WebWalterWrite <webwalterwhrite@gmail.com>
2019-04-25 14:06:23 +0200

    type: api desc: add doc api

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,20 +5,34 @@ const API_KEY = process.env.REACT_APP_FOOD_API_KEY;
 const URL = process.env.REACT_APP_FOOD_API_URL;
 
 /* Pour rechercher avec un ou plusieurs ingrédients.
-    parametre de fonction => "q" prend 1 ou plusieurs paramètres ( a ou a+b ou a+b+c ...): 
+
+INFOS API : 
+
+  https://developer.edamam.com/edamam-docs-recipe-api
+
+OPTIONS :
+  - cuisineType : Le type de cuisine de la recette. Vous pouvez utiliser simultanément cette cuisine "cuisineType = chinois &                   cuisineType = indien"
+  - dishType : Le type de plat auquel une recette appartient. Vous pouvez utiliser simultanément des types de plats saverals              de cette manière: "plat type = dîner et plat type = dessert"
+  
+  - health : Label de santé: L'un des paramètres d'api de santé répertoriés dans le tableau des étiquettes de régime et                  de santé à la fin de cette documentation. Les étiquettes Miltiple peuvent être soumises simultanément                       ainsi: «santé = sans arachides et santé = sans noix».
+  -q : prend 1 ou plusieurs paramètres ( a ou a+b ou a+b+c ...): 
         - Si chaine de caractères depuis le composant demandeur convertir en array => param = ...data
         - Sinon transmettre un array directement depuis le composant demandeur et donc param = data
-
-    "q" prend 1 ou plusieurs paramètres ( poulet / poulet+riz / poulet+riz+carottes)
+        "q" prend 1 ou plusieurs paramètres ( poulet / poulet+riz / poulet+riz+carottes)
 */
 
 export const getRecipes = async (...data) => {
+  try{
   // concatener les ingrédients si plusieurs choix.
   let choice = data.join("+");
 
   const result = await axios.get(
-    `${URL}?q=${choice}&app_id=${API_ID}&app_key=${API_KEY}&from=0&to=20`
+    `${URL}?&app_id=${API_ID}&app_key=${API_KEY}&q=${choice}&r&from=0&to=20`
   );
-  console.log(result);
+  console.log(result.data.hits);
   return result.data.hits;
+  }
+  catch(e){
+    console.log(e)
+  }
 };
