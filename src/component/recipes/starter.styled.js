ec78bb2415b3df0a288ab2a92a972f6620227824
WebWalterWrite <webwalterwhrite@gmail.com>
2019-04-25 14:05:45 +0200

    desc: add style Vegetables

--- src/component/recipes/starter.styled.js
+++ src/component/recipes/starter.styled.js
@@ -6,10 +6,11 @@ export const Vegetables = styled.div`
         flex-wrap:wrap;
         align-items: center;
         justify-content: space-around;
-        background-color: rgba(0,0,0,0.6);
+        background-color: rgba(0,0,0,0.4);
         div{
             text-align:center;
             padding-top:1em;
+            cursor: pointer;
             img{
                 width:150px;
                 height:150px;
