7f3e9aa909968fc8ad1e55e5bddef4d7b5007e1d
WebWalterWrite <webwalterwhrite@gmail.com>
2019-07-05 11:09:37 +0200

    type: data desc: data ingredients

--- src/component/recipes/data.js
+++ src/component/recipes/data.js
@@ -1,28 +1,63 @@
 // Récupérer toutes les images dans le dossier /vegetables
-const images = require.context("../../assets/images/vegetables/", true);
+const images = require.context("../../assets/images/", true);
 
 // tableau de légumes nommés
 const vegetable = [
-	"carrote",
-	"concombre",
-	"echalotte",
-	"poivron",
-	"pomme de terre",
-	"quinoa",
-	"salade",
-	"soja"
+	{name:"carrote",api_name:"carrot"},
+	{name:"concombre", api_name:"cucumber"},
+	{name:"echalotte", api_name:"shallot"},
+	{name:"gingembre", api_name:"ginger"},
+	{name:"poivron", api_name:"pepper"},
+	{name:"p. de terre", api_name:"potato"},
+	{name:"quinoa", api_name:"quinoa"},
+	{name:"salade", api_name:"lettuce"},
+	{name:"soja", api_name:"soybean"}
+];
+
+const meat = [
+	{name:'boeuf', api_name:"beef"},
+	{name:'crevette', api_name:"shrimp"},
+	{name:'jambon', api_name:'ham'},
+	{name:'poulet', api_name:"chicken"},
+	{name:'saumon', api_name:"salmon"},
+	{name:'thon', api_name:"tuna"}
+];
+
+const sauce = [
+	{name:'creme-fraiche', api_name:''},
+	{name:'sauce-tomate', api_name:'tomato sauce'},
+	{name:'sauce-poivre', api_name:"pepper sauce"}
 ];
 
 /**
  * @description - Tableau contenant les légumes
  * @return {Array} - Tableau d'objects
  */
-const vegetables = vegetable.map( v => ({
-		id: `v_${v.substr(0,3)}_${Math.floor(Math.random() * (30 - 10 + 1)) + 10}`,
-        name: v,
-        image: images(`./${v.split(" ").join("")}.jpg`),
+const vegetables = vegetable.map(({name, api_name}) => ({
+		id: `v_${name.substr(0,3)}_${Math.floor(Math.random() * (30 - 10 + 1)) + 10}`,
+		type:'v',
+		name: name,
+		api_name: api_name,
+        image: images(`./vegetables/${name.split(" ").join("")}.jpg`),
     })
  )
 
+const meats = meat.map( ({name, api_name}) =>  ({
+	id:  `name_${name.substr(0,3)}_${Math.floor(Math.random() * (30 - 10 + 1)) + 10}`,
+	type:'m',
+	name: name,
+	api_name: api_name,
+	image: images(`./meat/${name.split(" ").join("")}.jpg`),
+ }));
 
-export default vegetables;
+ const sauces = sauce.map( ({name, api_name}) =>({
+	id:`name_${name.substr(0,3)}_${Math.floor(Math.random() * (30 - 10 + 1)) + 10}`,
+	type:'s',
+	name:name,
+	api_name:api_name,
+	image: images(`./sauce/${name.split(" ").join("")}.jpg`),
+ }));
+
+
+
+ export const ingredients = vegetables.concat(meats, sauces);
