c9a4ddb75f9f4d138da542e90c23a03be3bb449a
WebWalterWrite <webwalterwhrite@gmail.com>
2019-07-05 15:43:58 +0200

    type: data desc: update data && add starchy array

--- src/component/recipes/data.js
+++ src/component/recipes/data.js
@@ -3,30 +3,37 @@ const images = require.context("../../assets/images/", true);
 
 // tableau de légumes nommés
 const vegetable = [
+	{name: 'brocolis', api_name:"broccoli"},
 	{name:"carrote",api_name:"carrot"},
 	{name:"concombre", api_name:"cucumber"},
 	{name:"echalotte", api_name:"shallot"},
 	{name:"gingembre", api_name:"ginger"},
+	{ name:"oignons", api_name:"onions"},
 	{name:"poivron", api_name:"pepper"},
-	{name:"p. de terre", api_name:"potato"},
-	{name:"quinoa", api_name:"quinoa"},
 	{name:"salade", api_name:"lettuce"},
 	{name:"soja", api_name:"soybean"}
 ];
 
+const starchy = [
+	{ name:"lentilles", api_name: "Lentils"},
+	{ name: "pates", api_name:"pasta"},
+	{ name: "p.deterre", api_name: "potato" },
+	{ name: "quinoa", api_name: "quinoa" },
+	{ name: "riz", api_name:"rice"}
+];
 const meat = [
-	{name:'boeuf', api_name:"beef"},
-	{name:'crevette', api_name:"shrimp"},
-	{name:'jambon', api_name:'ham'},
-	{name:'poulet', api_name:"chicken"},
-	{name:'saumon', api_name:"salmon"},
-	{name:'thon', api_name:"tuna"}
+	{name:"boeuf", api_name:"beef"},
+	{name:"crevette", api_name:"shrimp"},
+	{name:"jambon", api_name:"ham"},
+	{name:"poulet", api_name:"chicken"},
+	{name:"saumon", api_name:"salmon"},
+	{name:"thon", api_name:"tuna"}
 ];
 
 const sauce = [
-	{name:'creme-fraiche', api_name:''},
-	{name:'sauce-tomate', api_name:'tomato sauce'},
-	{name:'sauce-poivre', api_name:"pepper sauce"}
+	{name:"creme-fraiche", api_name:"fresh cream"},
+	{name:"sauce-tomate", api_name:"tomato sauce"},
+	{name:"sauce-poivre", api_name:"pepper sauce"}
 ];
 
 /**
@@ -41,6 +48,15 @@ const vegetables = vegetable.map(({name, api_name}) => ({
         image: images(`./vegetables/${name.split(" ").join("")}.jpg`),
     })
  )
+
+const starchys = starchy.map( ({name, api_name})=> ({
+		id: `f_${name.substr(0,3)}_${Math.floor(Math.random() * (30 - 10 + 1)) + 10}`,
+		type:'f',
+		name: name,
+		api_name: api_name,
+        image: images(`./starchy/${name.split(" ").join("")}.jpg`),
+    })
+);
 
 const meats = meat.map( ({name, api_name}) =>  ({
 	id:  `name_${name.substr(0,3)}_${Math.floor(Math.random() * (30 - 10 + 1)) + 10}`,
@@ -60,4 +76,4 @@ const meats = meat.map( ({name, api_name}) =>  ({
 
 
 
- export const ingredients = vegetables.concat(meats, sauces);
+ export const ingredients = vegetables.concat(meats, sauces, starchys);
