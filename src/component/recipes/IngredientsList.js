fa38e5ba0a57e9f4c9262aacabe6e5bcb7c2ca53
WebWalterWrite <webwalterwhrite@gmail.com>
2019-07-09 16:28:48 +0200

    type: Component desc: add new list ingredients

--- src/component/recipes/IngredientsList.js
+++ src/component/recipes/IngredientsList.js
@@ -5,38 +5,46 @@ import { Img, List } from './ingredients.styled';
 
 
 // tableau légumes
-const vegetables = ingredients.filter( ({type}) => type === 'v' );
+const vegetables = ingredients.filter(({ type }) => type === 'v');
+
+// tableau des herbes aromatiques
+const herbs = ingredients.filter(({ type }) => type === 'h');
 
 // tableau féculents
-const starchy = ingredients.filter( ({type}) => type === 'f');
+const starchy = ingredients.filter(({ type }) => type === 'f');
 // tableau viandes
-const meats = ingredients.filter( ({type}) => type === 'm' );
+const meats = ingredients.filter(({ type }) => type === 'm');
 
 // tableau sauces
-const sauces = ingredients.filter( ({type})=> type === "s");
+const sauces = ingredients.filter(({ type }) => type === "s");
 
 
 
-const ingredientsList = ({data: {t, fn}}) => {
+const ingredientsList = ({ data: { t, fn } }) => {
 
-    let type = t === "starchy" ? starchy : t === 'vegetables' ? vegetables : t === 'meats' ? meats : t === 'sauces' && sauces;
+    let type = 
+    t === "starchy" ? starchy : 
+    t === 'vegetables' ? vegetables : 
+    t === 'meats' ? meats : 
+    t === 'sauces' ? sauces :
+    t === 'herbs' && herbs;
 
-    return(
+    return (
         <List>
-        {type.map(({ image, name, id }, k) => {
-            return (
+            {type.map(({ image, name, id }, k) => {
+                return (
 
-             <div key={k}>
-                <div  data-id={id} data-type="v" onClick={fn} >
-                    <Img src={image}  alt="légumes" width="150" />
-                </div>
-                <p>{name}</p>
-                </div>
-            );
-        })}
-         </List>
+                    <div key={k}>
+                        <div data-id={id} data-type="v" onClick={fn} >
+                            <Img src={image} alt="légumes" width="150" />
+                        </div>
+                        <p>{name}</p>
+                    </div>
+                );
+            })}
+        </List>
 
-       
+
     )
 };
 
